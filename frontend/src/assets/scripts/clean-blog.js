e5cf8379b3b9c9c3fc8852ce55f1d2883a0b2e32
zen1st <zhe.zeng@ryerson.ca>
2018-01-11 11:06:57 -0500

    navs now fixed on top on mobile

--- frontend/src/assets/scripts/clean-blog.js
+++ frontend/src/assets/scripts/clean-blog.js
@@ -38,5 +38,38 @@ $(document).ready(function(){
         this.previousTop = currentTop;
       });
   }
+  else{
+	$('#mainNav').css("position", "fixed");
+  }
+  
+	$(window).on('resize', function(){
+	  if ($(window).width() > MQL) {
+		var headerHeight = $('#mainNav').height();
+		$(window).on('scroll', {
+			previousTop: 0
+		  },
+		  function() {
+			var currentTop = $(window).scrollTop();
+			//check if user is scrolling up
+			if (currentTop < this.previousTop) {
+			  //if scrolling up...
+			  if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
+				$('#mainNav').addClass('is-visible');
+			  } else {
+				$('#mainNav').removeClass('is-visible is-fixed');
+			  }
+			} else if (currentTop > this.previousTop) {
+			  //if scrolling down...
+			  $('#mainNav').removeClass('is-visible');
+			  if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+			}
+			this.previousTop = currentTop;
+		  });
+	  }
+	  else{
+		$('#mainNav').css("position", "fixed");
+	  }
+	});
+
 });
 //})(jQuery); // End of use strict
